655cba72f66aea8610eb53d37cd2176d966301c2
RenishMetto <81855119+RenishMetto@users.noreply.github.com>
2021-10-06 18:10:01 +0300

    This is my first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print(2<3)
+
+print(2-3+4>5)
+
+print(False != True)
+
+#IF ELIF ELSE
+
+age = input('Input your age: ')
+
+if int(age) == 20:
+    print('You are '+ age)
+else:
+    print('You are not '+ age)
+
