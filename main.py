cf1db9eb054994c3179c85a4b9845da36aa0e91a
RenishMetto <81855119+RenishMetto@users.noreply.github.com>
2021-10-12 18:17:44 +0300

    Lessons till 12th October

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(2<3)
+'''print(2<3)
 
 print(2-3+4>5)
 
@@ -11,5 +11,56 @@ age = input('Input your age: ')
 if int(age) == 20:
     print('You are '+ age)
 else:
-    print('You are not '+ age)
+    print('You are not '+ age)'''
+
+'''     Variables Exercise
+  - Create a variable named carname and assign the value Volvo to it
+carname = "Volvo"
+print(carname)
+
+  #- Create a variable called z, assign x + y to it, and display the result.
+x = 4
+y = 3
+z = x + y
+print(z)
+
+2. If else Exercise
+
+Print "Hello World" if a is not equal to b.
+a = 14
+b = 16
+
+if a != b:
+    print("Hello World")
+
+3. While loops Exercise
+
+Print I as long as I is less than 6.
+i = 0
+while i < 6:
+
+    print("i")
+    i = i + 1
+exit()
+
+#12/10/2021   LISTS AND TUPLES
+
+fruits = ["apple","oranges", 3]
+position = (2,3)
+color = (255,255,255)
+
+print(type(color))'''
+
+words = input("Type something: ")
+print(words.split())
+
+
+
+
+
+
+
+
+
+
 
